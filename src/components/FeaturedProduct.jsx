33a8cd33789a42a74b905bf1419cc06ffedc95ca
qwamedrew <ndrwsamankwa@gmail.com>
2025-05-08 10:54:06 +0000

    presentation edit

--- src/components/FeaturedProduct.jsx
+++ src/components/FeaturedProduct.jsx
@@ -6,7 +6,7 @@ const FeaturedProducts = ({ products, onInquire, onProductSelect }) => {
   const featuredProducts = products.slice(0, 4);
   
   return (
-    <section className="py-12 bg-gray-50">
+    <section className="py-12 mt-15 bg-gray-50">
       <div className="container mx-auto px-4">
         <div className="text-center mb-10">
           <h2 className="text-3xl font-bold text-gray-800">Featured Products</h2>
